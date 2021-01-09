0ff96ded99fec78719cccdeb9d5c98d13897d8bf
David J. Lee <djl5@williams.edu>
2021-01-09 14:49:52 +0900

    implement rank and select, multiblock select

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+#![feature(asm)]                // Use assembly implementations of rank and select
+
 mod util;
 use std::cmp;
 use util::B64;
+use util::B128;
+use murmur3;
+
+type Rem = u8;
 
 #[derive(Debug)]
 struct Block {
-    remainders: [u8; 64],
-    occupieds: B64,
-    runends: B64,
+    remainders: [Rem; 64],
+    occupieds: u64,
+    runends: u64,
     offset: u8,
 }
 
@@ -14,42 +20,47 @@ struct Block {
 struct Filter {
     // Data
     blocks: Vec<Block>,
+    nblocks: usize,
+    nslots: usize,
 
     // Sizes
     p: usize,                   // Fingerprint size
     q: usize,                   // Quotient size
     r: usize,                   // Remainder size
-    nslots: usize,
     
     // Misc
-    seed: i32,
+    seed: u32,
 }
+
+/// Error type for when multiblock_select fails to find the rank-th bit
+#[derive(Debug, Clone)]
+struct SelectOverflow;
 
 impl Block {
     fn new() -> Block {
         Block {
             remainders: [0; 64],
-            occupieds: B64::zero(),
-            runends: B64::zero(),
+            occupieds: 0,
+            runends: 0,
             offset: 0,
         }
     }
     fn is_occupied(&self, i: usize) -> bool {
-        self.occupieds.get(i)
+        B64::get(self.occupieds, i)
     }
     fn set_occupied(&mut self, to: bool, i: usize) {
-        self.occupieds.set_to(to, i);
+        self.occupieds = B64::set_to(self.occupieds, to, i);
     }
     fn is_runend(&self, i: usize) -> bool {
-        self.runends.get(i)
+        B64::get(self.runends, i)
     }
     fn set_runend(&mut self, to: bool, i: usize) {
-        self.runends.set_to(to, i);
+        self.runends = B64::set_to(self.runends, to, i);
     }
 }
 
 impl Filter {
-    fn new(n: usize, r: usize, seed: i32) -> Filter {
+    fn new(n: usize, r: usize, seed: u32) -> Filter {
         let nslots = cmp::max(64, util::nearest_pow_of_2(n));
         let nblocks = nslots/64;
         let q = (nslots as f64).log2() as usize;
@@ -60,11 +71,73 @@ impl Filter {
 
         Filter {
             blocks,
+            nblocks, 
+            nslots,
             q,
             r,
             p: q + r,
-            nslots,
             seed,
+        }
+    }
+    fn hash(&self, word: &str) -> u128 {
+        let ref mut b = word.as_bytes();
+        match murmur3::murmur3_x64_128(b, self.seed) {
+            Ok(v) => v,
+            Err(_) => panic!("Failed to hash word"),
+        }
+    }
+    fn calc_quot(&self, hash: u128) -> usize {
+        (hash & ((1_u128 << self.q)-1)) as usize
+    }
+    /// Get the k-th remainder for the hash
+    fn calc_rem(&self, hash: u128, k: usize) -> Rem {
+        // Clamp k to [0, r)
+        let k = if k >= self.r {
+            k % self.r
+        } else {
+            k
+        };
+        // Get k-th chunk of r bits:
+        // bits in [a, b) where a=q+kr, b=q+(k+1)r
+        let a = self.q + k * self.r;
+        let b = a + self.r;
+        assert!(b > 128, "Remainder chunk overflowed 128 bits");
+        let rem: Rem = ((hash & B128::half_open(a,b)) >> a) as Rem;
+        // Don' let hash be 0
+        if rem == 0 {
+            rem + 1
+        } else {
+            rem 
+        }
+    }
+
+    /// Finds the absolute index of the rank-th 1 bit in runends
+    /// past the start of the block_i-th block.
+    /// Note: rank indexes from 1.
+    fn multiblock_select(&self, block_i: usize, rank: usize) -> Result<usize, SelectOverflow> {
+        assert!(block_i < self.nblocks, "Block index out of bounds");
+        assert!(rank > 0, "Rank must index from 1");
+
+        let mut rank = rank;
+        let mut b = &self.blocks[block_i];
+        let mut s: usize;
+        let mut l: usize = block_i * 64;
+
+        loop {
+            s = util::bitselect(b.runends, (rank-1) as u64) as usize;
+            l += s;
+            rank -= util::popcnt(b.runends) as usize;
+            b = &self.blocks[l/64];
+            
+            if !(s==64 && l < self.nslots) {
+                break;
+            }
+        }
+
+        if l >= self.nslots {
+            Err(SelectOverflow {})
+        } else {
+            Ok(l)
         }
     }
 }
@@ -76,9 +149,9 @@ fn main() {
     let filter = Filter::new(64*2+1, 4, 0);
     println!("filter: {:?}", filter);
 
-    let mut b = B64::zero();
-    b.set_to(true, 0);
+    let b = 0_u64;
+    B64::set_to(b, true, 0);
     println!("bitarray: {:x?}", b);
-    b.set_to(true, 63);
+    B64::set_to(b, true, 63);
     println!("bitarray: {:x?}", b);
 }

fa63b7d7f352668ba4a10195801b696ff137f6c7
David J. Lee <djl5@williams.edu>
2021-01-16 22:54:25 +0900

    use RankSelectResult instead of Option; add a few tests for first_unused

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,15 @@ struct Filter {
     seed: u32,
 }
 
+/// The possible results of the rank_select function
+#[derive(Debug, PartialEq, Eq)]
+enum RankSelectResult {
+    Empty,                      // home slot unoccupied
+    Full(usize),           // home slot occupied, last filled loc
+    Overflow,                   // search went off the edge
+}
+use RankSelectResult::*;
+
 impl Block {
     fn new() -> Block {
         Block {
@@ -57,6 +66,7 @@ impl Block {
 }
 
 impl Filter {
+
     fn new(n: usize, r: usize) -> Filter {
         Self::new_seeded(n, r, 0)
     }
@@ -82,11 +92,17 @@ impl Filter {
     fn remainder(&self, x: usize) -> Rem {
         self.blocks[x/64].remainders[x%64]
     }
-    fn runend(&self, x: usize) -> bool {
+    fn is_runend(&self, x: usize) -> bool {
         self.blocks[x/64].is_runend(x%64)
     }
-    fn occupied(&self, x: usize) -> bool {
+    fn is_occupied(&self, x: usize) -> bool {
         self.blocks[x/64].is_occupied(x%64)
+    }
+    fn set_runend(&mut self, to: bool, x: usize) {
+        self.blocks[x/64].set_runend(to, x%64);
+    }
+    fn set_occupied(&mut self, to: bool, x: usize) {
+        self.blocks[x/64].set_occupied(to, x%64);
     }
     fn hash(&self, word: &str) -> u128 {
         let ref mut b = word.as_bytes();
@@ -149,10 +165,19 @@ impl Filter {
     }
 
     /// Performs the blocked equivalent of the unblocked operation
-    ///   select(Q.runends, rank(Q.occupieds, x)).
-    /// Returns Some(loc) if loc >= x; otherwise, returns None.
-    fn rank_select(&self, x: usize) -> Option<usize> {
-        assert!(x < self.nslots, "Absolute index out of range");
+    ///   y = select(Q.runends, rank(Q.occupieds, x)).
+    /// Note: x indexes from 0.
+    ///
+    /// Return behavior:
+    /// - If y > x, returns Some(y)
+    /// - If y <= x, returns None
+    /// - If y runs off the edge, returns None 
+    fn rank_select(&self, x: usize) -> RankSelectResult {
+        // Exit early if x is obviously out of range
+        if x >= self.nslots {
+            return Overflow
+        }
+
         let mut block_i = x / 64;
         let slot_i = x % 64;
         let mut b = &self.blocks[block_i];
@@ -164,7 +189,7 @@ impl Filter {
         // (2) block offset is 0
         // (3) there are no runs before the slot in the block
         if !b.is_occupied(slot_i) && b.offset == 0 && rank == 0 {
-            None
+            Empty
         } else {
             // Skip ahead to the block that offset is pointing inside
             let offset = b.offset % 64;
@@ -177,34 +202,48 @@ impl Filter {
             } else {
                 0
             };
-            // If rank(Q.occupieds, x) == 0, then select gives an invalid result
+            // If rank(Q.occupieds, x) == 0, then there's nothing to see here
             if rank == 0 {
-                None
+                Empty
             } else {
                 // (rank-1) accounts for select's indexing from 0
                 match self.multiblock_select(block_i, (rank-1) as usize) {
                     Some(loc) =>
-                        if loc < x {
-                            None
-                        } else {
-                            Some(loc)
+                        if loc < x { 
+                            Empty 
+                        } else { 
+                            Full(loc) 
                         },
-                    None => None,
+                    None => Overflow,
                 }
             }
         }
     }
     /// Finds the first unused slot at or after absloc x.
-    fn first_unused_slot(&self, x: usize) -> usize {
+    /// Returns None if no slot found; 
+    /// otherwise, returns Some(y) where y is the first open slot
+    fn first_unused_slot(&self, x: usize) -> Option<usize> {
         let mut x = x;
-        while let Some(loc) = self.rank_select(x) {
-            if x <= loc {
-                x = loc + 1;
-            } else {
-                break;
+        loop {
+            match self.rank_select(x) {
+                Empty => break Some(x),
+                Full(loc) =>
+                    if x <= loc {
+                        x = loc + 1;
+                    } else {
+                        break Some(x);
+                    }
+                Overflow => break None,
             }
         }
-        x
+        // while let Full(loc) = self.rank_select(x) {
+        //     if x <= loc {
+        //         x = loc + 1;
+        //     } else {
+        //         break;
+        //     }
+        // }
+        // Some(x)
     }
     /// Returns 1 if word is in the filter, 0 otherwise.
     fn contains(&self, word: &str) -> bool {
@@ -212,10 +251,10 @@ impl Filter {
         let quot = self.calc_quot(hash);
         let rem = self.calc_rem(hash, 0); // TODO: get 0-th rem for now
         
-        if !self.occupied(quot) {
+        if !self.is_occupied(quot) {
             false
         } else {
-            if let Some(mut loc) = self.rank_select(quot) {
+            if let Full(mut loc) = self.rank_select(quot) {
                 loop {
                     // If matching remainder found, return true
                     if self.remainder(loc) == rem {
@@ -226,7 +265,7 @@ impl Filter {
                         break false;
                     } else {
                         loc -= 1;
-                        if loc < quot || self.runend(loc) {
+                        if loc < quot || self.is_runend(loc) {
                             break false;
                         }
                     }
@@ -359,7 +398,7 @@ mod tests {
         {
             // No setup needed: filter blank by default
             for i in 0..64 {
-                assert_eq!(filter.rank_select(i), None)
+                assert_eq!(filter.rank_select(i), Empty)
             }
         }
         // Filter with one run
@@ -367,9 +406,9 @@ mod tests {
             let b = &mut filter.blocks[0];
             b.occupieds = 1;
             b.runends = 1;
-            assert_eq!(filter.rank_select(0), Some(0));
+            assert_eq!(filter.rank_select(0), Full(0));
             for i in 1..64 {
-                assert_eq!(filter.rank_select(i), None, "i={}", i);
+                assert_eq!(filter.rank_select(i), Empty, "i={}", i);
             }
         }
         // Filter with multiple runs
@@ -378,67 +417,113 @@ mod tests {
             // First two runs each have two elts, third run has one elt
             b.occupieds = 0b101001;
             b.runends   = 0b110010;
-            assert_eq!(filter.rank_select(0), Some(1));
-            assert_eq!(filter.rank_select(1), Some(1));
-            assert_eq!(filter.rank_select(2), None);
-            assert_eq!(filter.rank_select(3), Some(4));
-            assert_eq!(filter.rank_select(4), Some(4));
-            assert_eq!(filter.rank_select(5), Some(5));
+            assert_eq!(filter.rank_select(0), Full(1));
+            assert_eq!(filter.rank_select(1), Full(1));
+            assert_eq!(filter.rank_select(2), Empty);
+            assert_eq!(filter.rank_select(3), Full(4));
+            assert_eq!(filter.rank_select(4), Full(4));
+            assert_eq!(filter.rank_select(5), Full(5));
             for i in 6..64 {
-                assert_eq!(filter.rank_select(i), None);
+                assert_eq!(filter.rank_select(i), Empty);
             }
         }
     }
     #[test]
-    fn test_rank_select_multi_block() {
+    fn test_rank_select_multi_block_1() {
         // Filter with run starting in block 0 and ending in block 1
-        {
-            let mut filter = Filter::new(64*3, 4);
-            let b0 = &mut filter.blocks[0];
-            b0.occupieds = 1;
-            b0.runends = 0;
-            b0.offset = 64;     // position of b0[0]'s runend
+        let mut filter = Filter::new(64*3, 4);
+        let b0 = &mut filter.blocks[0];
+        b0.occupieds = 1;
+        b0.runends = 0;
+        b0.offset = 64;     // position of b0[0]'s runend
 
-            let b1 = &mut filter.blocks[1];
-            b1.occupieds = 0;
-            b1.runends = 1;
-            b1.offset = 1;      // position where b1's first run's end should go,
-                                // i.e., position after runend from b0
-            
-            for i in 0..64 {
-                assert_eq!(filter.rank_select(i), Some(64), "i={}", i);
-            }
-            for i in 65..filter.nslots {
-                assert_eq!(filter.rank_select(i), None, "i={}", i);
-            }
+        let b1 = &mut filter.blocks[1];
+        b1.occupieds = 0;
+        b1.runends = 1;
+        b1.offset = 1;      // position where b1's first run's end should go,
+        // i.e., position after runend from b0
+        
+        for i in 0..64 {
+            assert_eq!(filter.rank_select(i), Full(64), "i={}", i);
         }
+        for i in 65..filter.nslots {
+            assert_eq!(filter.rank_select(i), Empty, "i={}", i);
+        }
+    }
+    #[test]
+    fn test_rank_select_multi_block_2() {
         // Filter with two runs:
         // A run starts in b0 and ends in b1, making b1 have nonzero offset
-        {
-            let mut filter = Filter::new(64*3, 4);
-            let b0 = &mut filter.blocks[0];
-            b0.occupieds = 0b11;
-            b0.runends = 0b01;
-            b0.offset = 0;
+        let mut filter = Filter::new(64*3, 4);
+        let b0 = &mut filter.blocks[0];
+        b0.occupieds = 0b11;
+        b0.runends = 0b01;
+        b0.offset = 0;
 
-            let b1 = &mut filter.blocks[1];
-            b1.occupieds = 0b10;
-            b1.runends = 0b11;
-            b1.offset = 1;
-            
-            assert_eq!(filter.rank_select(0), Some(0));
-            assert_eq!(filter.rank_select(1), Some(64));
-            for i in 2..65 {
-                assert_eq!(filter.rank_select(i), Some(64), "i={}", i);
-            }
-            assert_eq!(filter.rank_select(65), Some(65));
-            for i in 66..filter.nslots {
-                assert_eq!(filter.rank_select(i), None, "i={}", i);
-            }
+        let b1 = &mut filter.blocks[1];
+        b1.occupieds = 0b10;
+        b1.runends = 0b11;
+        b1.offset = 1;
+        
+        assert_eq!(filter.rank_select(0), Full(0));
+        assert_eq!(filter.rank_select(1), Full(64));
+        for i in 2..65 {
+            assert_eq!(filter.rank_select(i), Full(64), "i={}", i);
+        }
+        assert_eq!(filter.rank_select(65), Full(65));
+        for i in 66..filter.nslots {
+            assert_eq!(filter.rank_select(i), Empty, "i={}", i);
         }
     }
     #[test]
-    // Empty single-block filter 
+    fn test_first_unused_slot() {
+        {
+            // Empty filter
+            // The first unused slot for a slot x should be x itself
+            let filter = Filter::new(128, 4);
+            for i in 0..filter.nslots {
+                assert_eq!(filter.first_unused_slot(i), Some(i), "i={}", i);
+            }
+        }
+        {
+            // Filter with one element at k
+            // The first unused slot for a slot x should be 
+            // k+1 if x == k, x otherwise (except when x=k=nslots-1)
+            let nslots = 128;
+            for k in 0..nslots {
+                let mut filter = Filter::new(nslots, 4);
+                filter.set_occupied(true, k);
+                filter.set_runend(true, k);
+                for i in 0..nslots {
+                    assert_eq!(
+                        filter.first_unused_slot(i), 
+                        if i == k {
+                            if i == nslots-1 && k == nslots-1 { None }
+                            else { Some(k+1) }
+                        } else {
+                            Some(i)
+                        },
+                        "k={}, i={}", k, i,
+                    );
+                }
+            }
+            
+
+            
+        }
+        {
+            // Filter with one multi-elt run from a to b
+            // 
+        }
+
+        // Filter with two non-overlapping runs
+        // Filter with two overlapping runs in the same block
+        // Filter with a run in one block extending to the next block 
+        // Filter with two overlapping runs in different blocks (using offset)
+
+    }
+    #[test]
+    /// Empty single-block filter 
     fn test_contains_empty_single() {
         let filter = Filter::new(64, 4);
         for i in 0..64 {
@@ -446,9 +531,8 @@ mod tests {
             assert!(!filter.contains(&word), "word='{}'", word);
         }
     }
-
     #[test]
-    // 1-elt single-block filter
+    /// 1-elt single-block filter
     fn test_contains_one_single() {
         let mut filter = Filter::new(64, 4);
         let word = "apples";
@@ -464,9 +548,8 @@ mod tests {
         
         assert!(filter.contains("apples"));
     }
-
     #[test]
-    // Multi-elt single-block filter
+    /// Multi-elt single-block filter
     fn test_contains_multi_single() {
         let mut filter = Filter::new(64, 4);
         // I checked that these words' quot/rems don't conflict
